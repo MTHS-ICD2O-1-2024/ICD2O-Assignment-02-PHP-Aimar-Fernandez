03d09d4e596674bf69a0f7131317f958576f8169
Aimar Fernandez <aimar.fernandez1@stu.ocsb.ca>
2025-03-21 17:00:15 +0000

    added php code

---
+++ answer.php
@@ -0,0 +1,58 @@
+<!DOCTYPE html>
+<!--ICD2O Assignment #2 PHP-->
+<html lang="en-ca">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="volume answer, PHP" />
+  <meta name="keywords" content="ICD2O, mths" />
+  <meta name="author" content="Aimar Fernandez" />
+  <meta name="viewport" content="width+device-width, initial-scale=.1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.deep_orange-red.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Volume Program, PHP</title>
+</head>
+
+<body>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <br />
+      <div class="mdl_layout__header-row">
+        <span class="mdl-layout-title">Volume Program, PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div>
+        <img 
+          src="./images/volume-of-rectangular-prism.png" 
+          alt="rectangular-prism-volume-equation"
+        >
+      </div>
+      <br />
+      <div class="page-content">
+        <?php
+        $length = $_GET["length"];
+        $width = $_GET["width"];
+        $height = $_GET["height"];
+        echo "<p>The area is: " . $length * $width * $height .  "</p>";
+        ?>
+      </div>
+      <br />
+      <div class="page-content-answer">
+        <a href="./index.php"> Return ...<a>
+      </div>
+      <br />
+    </main>
+  </div>
+</body>
+
+</html>
--- index.php
+++ index.php
@@ -1,9 +1,87 @@
 <!DOCTYPE html>
-  <html>
-  <head>
-    <title>Title</title>
-  </head>
-  <body>
-    <?php echo '<p>Hello, World!, in PHP</p>'; ?>
-  </body>
+<!--ICD2O Assignment #2 PHP-->
+<html lang="en-ca">
+
+<head>
+  <meta charset="utf-8" />
+  <meta name="description" content="volume program, PHP" />
+  <meta name="keywords" content="ICD2O, mths" />
+  <meta name="author" content="Aimar Fernandez" />
+  <meta name="viewport" content="width+device-width, initial-scale=.1.0" />
+  <link
+    rel="stylesheet"
+    href="https://fonts.googleapis.com/icon?family=Material+Icons" />
+  <link
+    rel="stylesheet"
+    href="https://code.getmdl.io/1.3.0/material.deep_orange-red.min.css" />
+  <link rel="stylesheet" href="./css/style.css" />
+  <link rel="apple-touch-icon" sizes="180x180" href="apple-touch-icon.png" />
+  <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png" />
+  <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png" />
+  <link rel="manifest" href="site.webmanifest" />
+  <title>Volume Program, PHP</title>
+</head>
+
+<body>
+  <script defer src="https://code.getmdl.io/1.3.0/material.min.js"></script>
+  <div class="mdl-layout mdl-js-layout mdl-layout--fixed-header">
+    <header class="mdl-layout__header">
+      <br />
+      <div class="mdl_layout__header-row">
+        <span class="mdl-layout-title">Volume Program, PHP</span>
+      </div>
+    </header>
+    <main class="mdl-layout__content">
+      <div>
+        <img 
+          src="./images/volume-of-rectangular-prism.png" 
+          alt="rectangular-prism-volume-equation"
+        >
+      </div>
+      <br />
+      <h3>Enter the dimensions of the rectangular prism here (cm):</h3>
+      <form action="answer.php" method="GET">
+        <br />
+        <!-- Numeric Textfield with Floating Label -->
+        <div
+          class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+          <input
+            class="mdl-textfield__input" type="text"
+            pattern="-?[0-9]*(\.[0-9]+)?" name="length" />
+          <label class="mdl-textfield__label" for="base-input"> Length (cm)</label>
+          <span class="mdl-textfield__error">Input is not a number!</span>
+        </div>
+        <br />
+        <!-- Numeric Textfield with Floating Label -->
+        <div
+          class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+          <input
+            class="mdl-textfield__input" type="text"
+            pattern="-?[0-9]*(\.[0-9]+)?" name="width" />
+          <label class="mdl-textfield__label" for="base-input"> Width (cm)</label>
+          <span class="mdl-textfield__error">Input is not a number!</span>
+        </div>
+        <br />
+        <!-- Numeric Textfield with Floating Label -->
+        <div
+          class="mdl-textfield mdl-js-textfield mdl-textfield--floating-label">
+          <input
+            class="mdl-textfield__input" type="text"
+            pattern="-?[0-9]*(\.[0-9]+)?" name="height" />
+          <label class="mdl-textfield__label" for="base-input"> Height (cm)</label>
+          <span class="mdl-textfield__error">Input is not a number!</span>
+        </div>
+        <br />
+        <!-- Raised button with ripple -->
+        <button
+          class="mdl-button mdl-js-button mdl-button--raised mdl-js-ripple-effect"
+          type="submit">
+          Enter
+        </button>
+        <br />
+      </form>
+    </main>
+  </div>
+</body>
+
 </html>
